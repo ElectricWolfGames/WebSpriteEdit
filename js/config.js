4b14608089d452726b5c4f8f36748afa3cf65c00
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2026-03-08 09:49:40 +0000

    Refactor sprite editor into modular files

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const gridSize = 16;
+export const pixelSize = 24;
+
+export const palette = [
+    "#000000", "#FFFFFF", "#FF0000", "#00FF00",
+    "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"
+];
